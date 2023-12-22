03348b880c17f47c9c3630f75ffb681242ac8ec6
Matias Sombeck <matiasgent@gmail.com>
2023-12-22 19:23:25 -0300

    Update sistema columnas

--- destColumna.js
+++ destColumna.js
@@ -1,15 +1,7 @@
 function destColumna(event){
 
-   console.log("antes");
-   for(let i=1; i<5; i++){
-    console.log(eval("goal"+i));
-   }
    for(let i=1; i<8; i++){
-    console.log(eval("column"+i));
-   }
-
-   for(let i=1; i<8; i++){
-        if (event.target.id == ("imgColumn"+i) && eval("column"+i+".length") == 0 && deck[carta].number == 13) {
+        if (event.target.id == ("imgColumn"+i) && eval("column"+i).length == 0 && deck[carta].number == 13) {
             event.target.style.background = "";
             elementoArrastrado.parentNode.removeChild( elementoArrastrado );
 
@@ -17,22 +9,36 @@ function destColumna(event){
             let cardColDestino = document.getElementById("cardsColumn"+i);
             cardColDestino.insertAdjacentHTML("beforeend","<img src="+deck[carta].img+" id='column"+i+"card1' class='column"+i+"Class' draggable='true' style='position:absolute ; top:0%; left:0%; z-index:0'>");
             
-            
-        
             deckUpdate();
             colUpdate();
-
-        
-            
-            console.log("despues");
-            for(let i=1; i<5; i++){
-            console.log(eval("goal"+i));
-            }
-            for(let i=1; i<8; i++){
-            console.log(eval("column"+i));
-            }
         }
     }
+
+for(let i=1; i<8; i++){
+        let colorCartaCol = eval("column"+i+"[column"+i+".length-1]") && deck[eval("column"+i+"[column"+i+".length-1]")].color; 
+        let numCartaCol = deck[eval("column"+i+"[column"+i+".length-1]")] && deck[eval("column"+i+"[column"+i+".length-1]")].number
+        let colorCarta = deck[carta].color;
+        let numCarta = deck[carta].number;
+        
+        if(event.target.id == ("column"+i+"card"+(eval("column"+i).length)) && colorCarta != colorCartaCol &&  numCartaCol == numCarta + 1 && eval("column"+1).length > 0){
+            event.target.style.background = "";
+            elementoArrastrado.parentNode.removeChild( elementoArrastrado );
+                
+            eval("column"+i).push(carta);
+
+            let lastCard = document.getElementById(event.target.id);
+            let zindexCard = parseInt(window.getComputedStyle(lastCard).getPropertyValue('z-index')) + 1;
+            let percentTop = parseInt(window.getComputedStyle(lastCard).getPropertyValue('top'))+15;
+            console.log(zindexCard);
+            console.log(percentTop);
+            let insHtmlDest = document.getElementById("cardsColumn"+i);
+            insHtmlDest.insertAdjacentHTML("beforeend","<img src="+deck[carta].img+" id='column"+i+"card"+eval("column"+i).length+"' class='column"+i+"Class' draggable='true' style='position:absolute ; top:"+percentTop+"px; left:0%; z-index:"+zindexCard+"'>");
+
+            deckUpdate();
+            colUpdate();
+        }
+
+}
 /*
 if (event.target.id == "column1card"+column1.length ) {
     event.target.style.background = "";
--- repartir.js
+++ repartir.js
@@ -17,11 +17,11 @@ for(let j=7; j>0; j--){
     for(var i=0; i<j; i++){ 
         let icol = eval("column"+j); 
         if(i+1 == icol.length){
-            col.insertAdjacentHTML("beforeend","<img src="+deck[eval("column"+j+"[i]")].img+" id='column"+(i+1)+"card"+j+"' ondragover='event' ondrop='event' class='column"+(i+1)+"Class' draggable='true' style='position:absolute;top:"+zPercent+"%;left:0%;z-index:"+i+";'>")
-            zPercent += 10;
+            col.insertAdjacentHTML("beforeend","<img src="+deck[eval("column"+j+"[i]")].img+" id='column"+(i+1)+"card"+j+"' ondragover='event' ondrop='event' class='column"+(i+1)+"Class' draggable='true' style='position:absolute;top:"+zPercent+"px;left:0%;z-index:"+i+";'>")
+            zPercent += 15;
         } else {
-            col.insertAdjacentHTML("beforeend","<img src='/public/cards/blue2.png' id='column"+(j)+"card"+(i+1)+"' class='column"+(j)+"Back' draggable='false' style='position:absolute;top:"+zPercent+"%;left:0%;z-index:"+i+";'>")
-            zPercent += 10; 
+            col.insertAdjacentHTML("beforeend","<img src='/public/cards/blue2.png' id='column"+(j)+"card"+(i+1)+"' class='column"+(j)+"Back' draggable='false' style='position:absolute;top:"+zPercent+"px;left:0%;z-index:"+i+";'>")
+            zPercent += 15; 
         }
     }
 }  
